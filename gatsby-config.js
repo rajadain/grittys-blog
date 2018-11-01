4aeb4836cb3e5cfdd032e669bf726bb9ab9f2258
Terence Tuhinanshu <terence@tuhinanshu.com>
2018-11-01 10:11:53 -0400

    Configure metadata, favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
+    title: "Gritty's Blog",
+    author: 'Terence Tuhinanshu',
+    description: 'LibertyJS Gatsby Workshop',
     siteUrl: 'https://ivanaveliskova.github.io',
   },
   pathPrefix: '/grittys-blog',
@@ -54,7 +54,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/assets/flyers-logo.png`,
       },
     },
     `gatsby-plugin-offline`,

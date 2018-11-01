ac21adda6ab8b9be83f13e2ee7b143322e38fcca
Terence Tuhinanshu <terence@tuhinanshu.com>
2018-11-01 10:44:57 -0400

    Fix date format

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -53,7 +53,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "MMMM DD, YYYY")
             title
           }
         }

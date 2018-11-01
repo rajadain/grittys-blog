1a9812014d3e578adfd2f2f728b6f4ce2952c6f7
Terence Tuhinanshu <terence@tuhinanshu.com>
2018-11-01 10:14:33 -0400

    Add header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import Header from './Header';
+
 class Template extends React.Component {
   render() {
     const { location, children } = this.props;
     const rootPath = `${__PATH_PREFIX__}/`;
-    let header;
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1>
-          <Link to={"/"}>Gatsby Starter Blog</Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3>
-          <Link to={"/"}>Gatsby Starter Blog</Link>
-        </h3>
-      );
-    }
     return (
       <div>
-        {header}
+        <Header />
         {children}
       </div>
     );
